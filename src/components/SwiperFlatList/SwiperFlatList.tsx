9aaac0143ed9d28d460f5d3a744fc5f23f4d8da3
BharatBhushan2907 <95857979+BharatBhushan2907@users.noreply.github.com>
2022-02-07 17:43:47 +0530

    Update SwiperFlatList.tsx
    allowing play pause feature

--- src/components/SwiperFlatList/SwiperFlatList.tsx
+++ src/components/SwiperFlatList/SwiperFlatList.tsx
@@ -167,7 +167,7 @@ export const SwiperFlatList = React.forwardRef(
       // https://upmostly.com/tutorials/settimeout-in-react-components-using-hooks
       return () => clearTimeout(autoplayTimer);
       // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [currentIndexes.index, _data.length]);
+    }, [currentIndexes.index, _data.length,autoplay]);
 
     const _onMomentumScrollEnd: FlatListProps<unknown>['onMomentumScrollEnd'] = (event) => {
       // NOTE: Method not executed when call "flatListElement?.current?.scrollToIndex"
